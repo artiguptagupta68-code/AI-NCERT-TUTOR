637bea0b256d562661d26ee265b0c42f929a8957
Arti Gupta <artigupta.gupta68@gmail.com>
2025-12-03 19:17:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+import gdown
+from pathlib import Path
+from pypdf import PdfReader
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import streamlit as st
+
+# ----------------------------
+# CONFIG
+# ----------------------------
+FILE_ID = "1gdiCsGOeIyaDlJ--9qon8VTya3dbjr6G"
+ZIP_PATH = "ncert.zip"
+EXTRACT_DIR = "ncert_extract
